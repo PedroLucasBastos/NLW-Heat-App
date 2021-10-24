2653f92aba7ebb0c1aa7ea1b8eee2d9b7420630d
Pedro Lucas <pedro19792001@gmail.com>
2021-10-24 20:55:50 -0300

    Tela de carregamento e inicio da tela principal

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+import LogoSvg from '../../assets/logo_teste.svg';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <LogoSvg />
+
+            <TouchableOpacity>
+                <Text style={styles.logoutText}>Sair</Text>
+            </TouchableOpacity>
+
+        </View>
+    );
+}
