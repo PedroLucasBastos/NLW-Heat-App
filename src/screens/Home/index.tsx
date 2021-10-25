5030ed4e358d8c6e5f2aaf2c8ca2c263ea8ec006
Pedro Lucas <pedro19792001@gmail.com>
2021-10-25 19:16:23 -0300

    finalizando componente de envio de MENSAGEM

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import { styles } from './styles';
 import { Header } from '../../components/Header'
 import { MessageList } from '../../components/MessageList';
 import { SingninBox } from '../../components/SingninBox';
+import { SendMessageForm } from '../../components/SendMessageForm'
 
 
 export function Home() {
@@ -14,7 +15,7 @@ export function Home() {
         <View style={styles.container}>
             <Header />
             <MessageList />
-            <SingninBox />
+            <SendMessageForm />
         </View >
     )
 }
