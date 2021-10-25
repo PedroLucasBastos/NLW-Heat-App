d87dfded0be9f875890ef0b8edc866c2654ca753
Pedro Lucas <pedro19792001@gmail.com>
2021-10-25 14:12:30 -0300

    header finalizada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-
 import { View, Text, TouchableOpacity } from 'react-native';
+
 
 import { styles } from './styles';
 import LogoSvg from '../../assets/logo_teste.svg';
+import { UserFoto } from '../UserFoto';
 
 export function Header() {
     return (
         <View style={styles.container}>
             <LogoSvg />
+            <View style={styles.logoutButton}>
+                <TouchableOpacity>
+                    <Text style={styles.logoutText}>Sair</Text>
+                </TouchableOpacity>
 
-            <TouchableOpacity>
-                <Text style={styles.logoutText}>Sair</Text>
-            </TouchableOpacity>
-
+                <UserFoto imageUri="https://github.com/PedroLucasBastos.png " />
+            </View>
         </View>
     );
 }
